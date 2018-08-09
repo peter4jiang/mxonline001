53ef0c749de6cba03ba73eb699ac5479ae828b72
peter4jiang <32516895+peter4jiang@users.noreply.github.com>
2018-08-09 22:32:21 +0800

    添加简单登录功能

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.contrib.auth import authenticate, login
 
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'index.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            pass
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
